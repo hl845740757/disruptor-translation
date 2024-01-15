98aae4d42e8cdabff51d9254eb4f34cf42611b9b
wjybxx <845740757@qq.com>
2024-01-15 23:08:45 +0800

    阻塞等待策略有坑

--- src/main/java/com/lmax/disruptor/BlockingWaitStrategy.java
+++ src/main/java/com/lmax/disruptor/BlockingWaitStrategy.java
@@ -22,6 +22,11 @@ import java.util.concurrent.locks.ReentrantLock;
 import com.lmax.disruptor.util.ThreadHints;
 
 /**
+ * 阻塞等待策略。
+ * 通过lock等待【生产者】发布数据，可以达到较低的cpu开销。
+ * 注意：有坑！Disruptor框架并没有消费者之间的协调策略，而是通过简单的忙等策略实现的，
+ * 因此，如果前置消费者消费较慢，而后置消费者速度较快，使用该策略反而会导致极大的开销，这种情况下可使用{@link SleepingWaitStrategy}。
+ *
  * Blocking strategy that uses a lock and condition variable for {@link EventProcessor}s waiting on a barrier.
  * <p>
  * This strategy can be used when throughput and low-latency are not as important as CPU resource.
